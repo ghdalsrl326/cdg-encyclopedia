7279fb3283dae55887e5bdc5a7a3ac587fe461fe
minkihong <ghdalsrl326@gmail.com>
2023-09-24 14:00:49 +0900

    커버 페이지 내용 채우기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import localFont from "next/font/local";
+import Link from "next/link";
+import { URL } from "@/data/constant";
 
 const yeongdoFont = localFont({
   src: "./fonts/yeongdo.woff2",
@@ -7,5 +9,44 @@ const yeongdoFont = localFont({
 });
 
 export default function Home() {
-  return <main className={yeongdoFont.className}>창덕도감 커버페이지</main>;
+  return (
+    <main className={yeongdoFont.className}>
+      <section className="flex flex-col items-center h-screen relative">
+        <h1 className="text-5xl z-10">창덕도감</h1>
+        <Image
+          fill
+          className="absolute top-0 left-0 z-0"
+          src="/cover/under.svg"
+          alt="background-image"
+          style={{ objectFit: "cover" }}
+          quality={100}
+        />
+        <Image
+          fill
+          className="absolute top-0 left-0 z-20"
+          src="/cover/over.svg"
+          alt="background-animation-object"
+          style={{ objectFit: "cover" }}
+          quality={100}
+        />
+        <Image
+          className="z-10"
+          src="/cover/인정전.svg"
+          alt="인정전"
+          width={839}
+          height={685}
+          quality={100}
+        />
+        <Link href={URL.VISITOR_INFO} className=" bg-white z-20">
+          지도보기
+        </Link>
+        <Link href={URL.BUILDING_INFO} className="w-1/8 bg-white z-20">
+          창덕궁 둘러보기
+        </Link>
+        <Link href={URL.GARDEN} className="w-1/8 bg-white z-20">
+          후원의 숲
+        </Link>
+      </section>
+    </main>
+  );
 }
---
+++ src/data/constant.ts
@@ -0,0 +1,6 @@
+export const URL = {
+  COVER: "/",
+  BUILDING_INFO: "/building-info",
+  GARDEN: "/garden",
+  VISITOR_INFO: "/visitor-info",
+};
