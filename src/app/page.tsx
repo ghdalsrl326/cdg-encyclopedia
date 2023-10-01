42dac59081b399ae6a1ed38bf31abd20aa2a13e4
minkihong <ghdalsrl326@gmail.com>
2023-10-01 16:08:05 +0900

    커버 페이지 반응형 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
   return (
     <main className={yeongdoFont.className}>
       <section className="relative flex flex-col justify-center items-center h-screen">
-        <h1 className="text-6xl text-white z-10">창덕도감</h1>
+        <h1 className="absolute top-[10%] text-6xl text-white z-10">
+          창덕도감
+        </h1>
         <Image
           fill
           className="absolute top-0 left-0 z-0"
@@ -32,21 +34,24 @@ export default function Home() {
           height={685}
           quality={100}
         />
-        <div className="absolute top-2/3 z-20 text-2xl w-[60%] text-[#FF61D6]">
-          <div className="flex flex-row gap-16 text-center">
+        <div className="absolute top-2/3 z-20 text-2xl w-full px-16 text-[#FF61D6]">
+          <div className="flex flex-col md:flex-row gap-4 md:gap-16 text-center">
             <Link
               href={URL.VISITOR_INFO}
-              className="w-1/3 p-4 bg-white rounded-full"
+              className="w-full md:w-1/3 p-4 bg-white rounded-full"
             >
               지도보기
             </Link>
             <Link
               href={URL.BUILDING_INFO}
-              className="w-1/3 p-4 bg-white rounded-full"
+              className="w-full md:w-1/3 p-4 bg-white rounded-full"
             >
               창덕궁 둘러보기
             </Link>
-            <Link href={URL.GARDEN} className="w-1/3 p-4 bg-white rounded-full">
+            <Link
+              href={URL.GARDEN}
+              className="w-full md:w-1/3 p-4 bg-white rounded-full"
+            >
               후원의 숲
             </Link>
           </div>
