cfa3ed4c8f1905b68ff559c768b3b21aaf9c74b0
minkihong <ghdalsrl326@gmail.com>
2023-10-01 16:07:02 +0900

    나무 설문 > 편지지 반응형 추가

--- src/components/GardenTwoButtonLayout.tsx
+++ src/components/GardenTwoButtonLayout.tsx
@@ -9,14 +9,16 @@ type Props = {
 
 const GardenTwoButtonLayout = ({ question, onYes, onNo }: Props) => {
   return (
-    <div className="flex flex-col justify-around items-center bg-white w-full h-1/2 z-10">
-      <Image
-        fill
-        src={`/garden/편지지.png`}
-        alt="편지지"
-        style={{ objectFit: "contain" }}
-        className="z-10"
-      ></Image>
+    <div className="flex flex-col justify-around items-center bg-white w-full h-1/2 z-10 relative">
+      <div className="absolute -inset-10 z-0">
+        <Image
+          fill
+          src={`/garden/편지지.png`}
+          alt="편지지"
+          style={{ objectFit: "cover", objectPosition: "center" }}
+          className="z-0"
+        />
+      </div>
       <Image
         className="z-10"
         src={`/garden/편지지나무.png`}
