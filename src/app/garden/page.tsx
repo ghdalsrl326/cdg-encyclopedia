bcc6ea072bdce8ede0c05e63956fa1f854dd2d25
minkihong <ghdalsrl326@gmail.com>
2023-10-07 16:25:58 +0900

    소원나무 툴팁 추가

--- src/app/garden/page.tsx
+++ src/app/garden/page.tsx
@@ -22,8 +22,8 @@ const page = async () => {
         <div className="grid gap-6 grid-cols-2 sm:grid-cols-2 md:grid-cols-5 lg:grid-cols-7 xl:grid-cols-12">
           {trees.map((tree, index) => (
             <div
-              key={index}
-              className="relative w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-24 lg:h-24 xl:w-20 xl:h-20 2xl:w-16 2xl:h-16"
+              key={tree._id}
+              className="group relative w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-24 lg:h-24 xl:w-20 xl:h-20 2xl:w-16 2xl:h-16"
             >
               <Image
                 src={`/garden/${tree.treeType}.png`}
@@ -31,6 +31,11 @@ const page = async () => {
                 layout="fill"
                 objectFit="cover"
               />
+              <div className="hidden group-hover:block absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-full bg-black text-white text-sm  rounded p-1 min-w-[100px] text-center">
+                {tree.name !== "" && `나무이름: ${tree.name}`}
+                <br />
+                {tree.description !== "" && `소원: ${tree.description}`}
+              </div>
             </div>
           ))}
         </div>
