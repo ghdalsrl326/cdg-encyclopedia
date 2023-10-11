fa142aa988e78c4995042073067502a32b4ffb5e
minkihong <ghdalsrl326@gmail.com>
2023-10-11 15:00:17 +0900

    img_5032 창덕궁 둘러보기 버튼, 레이스 fixed로 스타일 수정

--- src/app/building-info/page.tsx
+++ src/app/building-info/page.tsx
@@ -15,17 +15,17 @@ const page = () => {
     >
       <h1 className="text-xl z-10">건물을 눌러 정보를 확인하세요!</h1>
       <Image
-        className="absolute bottom-0 left-0 z-10 w-full"
+        className="fixed bottom-0 left-0 z-20 w-full"
         src="/building-info/backgroundFooter.png"
         alt="background-footer"
-        style={{ objectFit: "contain", objectPosition: "center" }}
+        style={{ position: "fixed", height: "10%", objectFit: "cover" }}
         quality={100}
-        width={1920}
-        height={1080}
+        width={3060}
+        height={164}
       />
       <BuildingGrid />
       <button
-        className="absolute bottom-20 z-20 bg-white rounded-full shadow-lg shadow-[#D3EEF3] w-48 h-12 text-xl"
+        className="fixed bottom-20 z-20 bg-white rounded-full shadow-lg shadow-[#D3EEF3] w-48 h-12 text-xl"
         disabled
       >
         창덕궁 둘러보기
